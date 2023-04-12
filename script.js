1f4afd21478440a1ccdd1768613d6d9450c2e9f9
MikePio <michelepiopilla@gmail.com>
2023-04-12 18:59:28 +0200

    completed exercise with bonus

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
+const {createApp} = Vue 
 
-
+createApp({
+    data(){
+        return{
+            message: 'Hello Vue',
+            //* Bonus  
+            imageTest: 'jpeg-home.jpg'
+        }
+    }
+}).mount('#app')
 
 
 
